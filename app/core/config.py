2238c29ce3b85f4fcc4de14e3cede34fb92e5a1b
Felipe Lazo <felipe.gutierrez280996@gmail.com>
2026-06-04 22:29:04 -0400

    Configuración de la conexión a la base de datos, creación de credenciales globales.

--- app/core/config.py
+++ app/core/config.py
@@ -1,30 +1,24 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-import os
-from dotenv import load_dotenv
-
-# 1. Load base configuration first
-load_dotenv(".env")
-
-# Access variables normally
-DB_USER = os.getenv("DB_USER")
-DB_PW = os.getenv("DB_PW")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_SCHEMA = os.getenv("DB_SCHEMA")
-
+from pydantic import computed_field
 class Settings(BaseSettings):
     # Nombre de tu API
     PROJECT_NAME: str = "Sistema de Reservas"
-    
-    # Configuración de Pydantic para que lea automáticamente un archivo .env si existe en la raíz
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
+    DB_USER: str
+    DB_PW: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_SCHEMA: str
+    
     # URL de conexión a la base de datos.
     # Al estar usando FastAPI (asíncrono) y PostgreSQL, el driver ideal es 'asyncpg'.
     # Formato: postgresql+asyncpg://usuario:password@host:puerto/nombre_bd
-    DATABASE_URL: str = f"postgresql+asyncpg://{DB_USER}:{DB_PW}@{DB_HOST}:{DB_PORT}/{DB_SCHEMA}"
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PW}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_SCHEMA}"
 
+    # Configuración de Pydantic para que lea automáticamente un archivo .env si existe en la raíz
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
 # Instanciamos la configuración para poder importarla en otros archivos como 'settings.DATABASE_URL'
 settings = Settings()
